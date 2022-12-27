565a568574e3f36cc0a3fe0b4534ce40078ceb3e
MemoryEditor <120334229+MemoryEditor@users.noreply.github.com>
2022-12-27 16:20:09 -0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -50,7 +50,6 @@ commands.goto = function(arguments)
 				local Character = workspace[player.Name]
 				local HumanoidRootPart = Character.HumanoidRootPart
 				HumanoidRootPart.CFrame = workspace[p.Name].HumanoidRootPart.CFrame
-				print("done")
 			end
 		end
 	end
@@ -61,17 +60,17 @@ commands.arrest = function(arguments)
 		for _,p in pairs(game:GetService("Players"):GetPlayers()) do
 			local _p = string.lower(p.Name)
 			if string.match(_p, v, 1) then
-				local repeatTP = 20
+				local repeatTP = 10
 				
 				while repeatTP >= 0 do
-				local Character = workspace[player.Name]
-				local HumanoidRootPart = Character.HumanoidRootPart
-				HumanoidRootPart.CFrame = workspace[p.Name].HumanoidRootPart.CFrame
-				repeatTP -= 1
+					local Character = workspace[player.Name]
+					local HumanoidRootPart = Character.HumanoidRootPart
+					HumanoidRootPart.CFrame = workspace[p.Name].HumanoidRootPart.CFrame
+					repeatTP -= 1
 				
-				local A_1 = game:GetService("Workspace")[p.Name].Torso
-				local Event = game:GetService("Workspace").Remote.arrest
-				Event:InvokeServer(A_1)
+					local A_1 = game:GetService("Workspace")[p.Name].Torso
+					local Event = game:GetService("Workspace").Remote.arrest
+					Event:InvokeServer(A_1)
 				end
 			end
 		end
