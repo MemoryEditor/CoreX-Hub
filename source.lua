f77e00c7e2aa6322c89fcdd281587c8211db0e5b
MemoryEditor <120334229+MemoryEditor@users.noreply.github.com>
2022-12-27 16:07:34 -0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -61,6 +61,10 @@ commands.arrest = function(arguments)
 		for _,p in pairs(game:GetService("Players"):GetPlayers()) do
 			local _p = string.lower(p.Name)
 			if string.match(_p, v, 1) then
+				local Character = workspace[player.Name]
+				local HumanoidRootPart = Character.HumanoidRootPart
+				HumanoidRootPart.CFrame = workspace[p.Name].HumanoidRootPart.CFrame
+				
 				local A_1 = game:GetService("Workspace")[p.Name].Torso
 				local Event = game:GetService("Workspace").Remote.arrest
 				Event:InvokeServer(A_1)
