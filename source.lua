cc25a3e2c9a1a380bb92c313f487e1fc7a609b6f
MemoryEditor <120334229+MemoryEditor@users.noreply.github.com>
2023-01-02 14:20:49 -0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -22,7 +22,7 @@ Background.Name = "Background"
 Background.Parent = CoreXLoader
 Background.BackgroundColor3 = Color3.fromRGB(22, 22, 22)
 Background.Position = UDim2.new(0.336762398, 0, 0.199519232, 0)
-Background.Size = UDim2.new(0.324999988, 0, 0.600000024, 0)
+Background.Size = UDim2.new(0, 334, 0, 249)
 
 UICorner.CornerRadius = UDim.new(0, 12)
 UICorner.Parent = Background
@@ -33,7 +33,7 @@ Credits.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
 Credits.BorderSizePixel = 0
 Credits.Position = UDim2.new(0.794314384, 0, 0.897435904, 0)
 Credits.Size = UDim2.new(0, 56, 0, 17)
-Credits.Font = Enum.Font.Ubuntu
+Credits.Font = Enum.Font.Unknown
 Credits.Text = "Credits"
 Credits.TextColor3 = Color3.fromRGB(204, 204, 204)
 Credits.TextSize = 14.000
@@ -53,7 +53,7 @@ TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
 TextLabel.Position = UDim2.new(0.0179168656, 0, 0.885416687, 0)
 TextLabel.Size = UDim2.new(0, 166, 0, 23)
-TextLabel.Font = Enum.Font.Ubuntu
+TextLabel.Font = Enum.Font.Unknown
 TextLabel.Text = "Made by MemoryEd1tor"
 TextLabel.TextColor3 = Color3.fromRGB(204, 204, 204)
 TextLabel.TextSize = 15.000
@@ -64,7 +64,7 @@ TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_2.BackgroundTransparency = 1.000
 TextLabel_2.Position = UDim2.new(0.0507644527, 0, 0.136217952, 0)
 TextLabel_2.Size = UDim2.new(0, 305, 0, 122)
-TextLabel_2.Font = Enum.Font.Ubuntu
+TextLabel_2.Font = Enum.Font.Unknown
 TextLabel_2.Text = "I made this script hub for new exploiters that don't know how to script to learn scripting so that's why all the code is open source. I do not like to call people skids just because they want to use the source code to learn, but if you will use something from my script hub, I ask you to give me some credit, thanks for using my script hub and have fun exploiting!"
 TextLabel_2.TextColor3 = Color3.fromRGB(204, 204, 204)
 TextLabel_2.TextSize = 15.000
@@ -79,7 +79,7 @@ Title.BackgroundTransparency = 1.000
 Title.Position = UDim2.new(0.0507644527, 0, 0, 0)
 Title.Size = UDim2.new(0, 305, 0, 34)
 Title.ZIndex = 2
-Title.Font = Enum.Font.Ubuntu
+Title.Font = Enum.Font.Unknown
 Title.Text = "CoreX Credits"
 Title.TextColor3 = Color3.fromRGB(204, 204, 204)
 Title.TextSize = 25.000
@@ -90,7 +90,7 @@ Close.Parent = CreditsFrame
 Close.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Close.Position = UDim2.new(0.017916888, 0, 0.0200320482, 0)
 Close.Size = UDim2.new(0, 24, 0, 23)
-Close.Font = Enum.Font.Ubuntu
+Close.Font = Enum.Font.Unknown
 Close.Text = "X"
 Close.TextColor3 = Color3.fromRGB(204, 204, 204)
 Close.TextSize = 20.000
@@ -102,7 +102,7 @@ Title_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Title_2.BackgroundTransparency = 1.000
 Title_2.Position = UDim2.new(0.0388198756, 0, 0, 0)
 Title_2.Size = UDim2.new(0, 310, 0, 34)
-Title_2.Font = Enum.Font.Ubuntu
+Title_2.Font = Enum.Font.Unknown
 Title_2.Text = "CoreX Loader"
 Title_2.TextColor3 = Color3.fromRGB(204, 204, 204)
 Title_2.TextSize = 25.000
@@ -117,7 +117,7 @@ PrisonLife.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
 PrisonLife.BorderSizePixel = 0
 PrisonLife.Position = UDim2.new(0.0388198756, 0, 0.228365391, 0)
 PrisonLife.Size = UDim2.new(0, 94, 0, 20)
-PrisonLife.Font = Enum.Font.Ubuntu
+PrisonLife.Font = Enum.Font.Unknown
 PrisonLife.Text = "Prison Life"
 PrisonLife.TextColor3 = Color3.fromRGB(204, 204, 204)
 PrisonLife.TextSize = 15.000
@@ -129,7 +129,7 @@ MM2.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
 MM2.BorderSizePixel = 0
 MM2.Position = UDim2.new(0.349378884, 0, 0.228365377, 0)
 MM2.Size = UDim2.new(0, 53, 0, 20)
-MM2.Font = Enum.Font.Ubuntu
+MM2.Font = Enum.Font.Unknown
 MM2.Text = "MM2"
 MM2.TextColor3 = Color3.fromRGB(204, 204, 204)
 MM2.TextSize = 15.000
@@ -141,13 +141,13 @@ Title_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Title_3.BackgroundTransparency = 1.000
 Title_3.Position = UDim2.new(0, 0, 0.136217952, 0)
 Title_3.Size = UDim2.new(0, 130, 0, 23)
-Title_3.Font = Enum.Font.Ubuntu
+Title_3.Font = Enum.Font.Unknown
 Title_3.Text = "Supported Games"
 Title_3.TextColor3 = Color3.fromRGB(204, 204, 204)
 Title_3.TextSize = 15.000
 Title_3.TextWrapped = true
 
-local function DWXSLG_fake_script()
+local function IQHGYNR_fake_script()
 	local script = Instance.new('LocalScript', Credits)
 
 	local credits = script.Parent
@@ -162,8 +162,8 @@ local function DWXSLG_fake_script()
 		frame.Visible = false
 	end)
 end
-coroutine.wrap(DWXSLG_fake_script)()
-local function AGUEO_fake_script()
+coroutine.wrap(IQHGYNR_fake_script)()
+local function NWXT_fake_script()
 	local script = Instance.new('LocalScript', SupportedGames)
 
 	local supportedGamesFolder = script.Parent
@@ -182,8 +182,8 @@ local function AGUEO_fake_script()
 		end
 	end)
 end
-coroutine.wrap(AGUEO_fake_script)()
-local function AXMMN_fake_script()
+coroutine.wrap(NWXT_fake_script)()
+local function GJGRIBR_fake_script()
 	local script = Instance.new('LocalScript', Background)
 
 	local Drag = script.Parent
@@ -226,4 +226,4 @@ local function AXMMN_fake_script()
 		end)
 	
 end
-coroutine.wrap(AXMMN_fake_script)()
+coroutine.wrap(GJGRIBR_fake_script)()
